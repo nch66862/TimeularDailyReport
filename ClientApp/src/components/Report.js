31f6b4ea78a141ec15b658f3258cc787234eb904
Nick Carver <nicholascarver2@outlook.com>
2021-11-11 18:34:00 -0600

    i think i have the spacing just right

--- ClientApp/src/components/Report.js
+++ ClientApp/src/components/Report.js
@@ -22,31 +22,52 @@ export const Report = () => {
 
     const divStyle = {
         display: 'flex',
-        padding: '20px'
+        padding: '15px'
+    };
+
+    const timeDivStyle = {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        width: '25%'
+    };
+
+    const heiphenStyle = {
+        paddingLeft: '5px',
+        paddingRight: '5px'
     };
 
     const evenSpacing = {
         width: '15%'
     }
 
+    const timeHeader = {
+        width: '25%'
+    }
+
+    const activityNoteSpacing = {
+        width: '28%'
+    }
+
     return (
         <div>
             <h1>{todaysDate}</h1>
             <div style={divStyle}>
-                <div style={evenSpacing}>Time</div>
-                <div style={evenSpacing}>Duration</div>
-                <div style={evenSpacing}>Activity</div>
-                <div style={evenSpacing}>Note</div>
+                <h3 style={timeHeader}>Time</h3>
+                <h3 style={evenSpacing}>Duration</h3>
+                <h3 style={activityNoteSpacing}>Activity</h3>
+                <h3 style={activityNoteSpacing}>Note</h3>
             </div>
             {reportData?.timeEntries?.map(entry => {
                 return (
                     <div key={entry.id} id={entry.id} style={divStyle}>
-                        <div style={evenSpacing}><Time time={entry.duration.startedAt} /></div>
-                        <h5 style={evenSpacing}> - </h5>
-                        <div style={evenSpacing}><Time time={entry.duration.stoppedAt} /></div>
-                        <div style={evenSpacing}><TotalTime timeStarted={entry.duration.startedAt} timeEnded={entry.duration.stoppedAt} /></div>
-                        <h5 style={evenSpacing}>{activities.activities.find(a => a.id == entry.activityId)?.name}</h5>
-                        <h5 style={evenSpacing}>{entry.note.text}</h5>
+                        <div style={timeDivStyle}>
+                            <h5><Time time={entry.duration.startedAt} /></h5>
+                            <h5 style={heiphenStyle}> - </h5>
+                            <h5><Time time={entry.duration.stoppedAt} /></h5>
+                        </div>
+                        <h5 style={evenSpacing}><TotalTime timeStarted={entry.duration.startedAt} timeEnded={entry.duration.stoppedAt} /></h5>
+                        <h5 style={activityNoteSpacing}>{activities.activities.find(a => a.id == entry.activityId)?.name}</h5>
+                        <h5 style={activityNoteSpacing}>{entry.note.text}</h5>
                     </div>
                 )
             })}
