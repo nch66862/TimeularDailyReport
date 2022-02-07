ad11a77f47ea33c9a644f38ed50ab7fa6e3495a5
Nick Carver <nicholascarver2@outlook.com>
2022-02-06 19:40:11 -0600

    got the api key and api secret inputs to show up on the screen

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -12,14 +12,27 @@ export const Home = () => {
       apiKey: "MTMxNjA5XzE1NWFmOWJkNzMwZjQyZmJhOWFkNGUyM2I1YjZmYTZm",
       apiSecret: "MjNkNWEyMGI3MDQ5NDQ5YWE2NDRlZmIwMDAyYTE2MTU="
     })
+  const [chosenDate, setChosenDate] = useState(getCurrentDate())
 
   const HandleLogin = () => {
     setLogInDisabled(true)
     getAPIToken(loginCredentials)
   }
 
-  const ChangeAPIKey = () => {
-    const 
+  const ChangeAPIKey = (newApiKey) => {
+    let credentials = { ...loginCredentials }
+    credentials.apiKey = newApiKey
+    setLoginCredentials(credentials)
+  }
+
+  const ChangeAPISecret = (newApiSecret) => {
+    let credentials = { ...loginCredentials }
+    credentials.apiSecret = newApiSecret
+    setLoginCredentials(credentials)
+  }
+
+  const ChangeChosenDate = (datetime) => {
+    
   }
 
   const HandleDailyReport = () => {
@@ -28,11 +41,27 @@ export const Home = () => {
       .then(() => setDailyReportEnabled(true))
   }
 
+  const getCurrentDate = () => {
+    const d = new Date()
+    const z = (n) => {
+      return (n < 10 ? '0' : '') + n
+    }
+    return d.getFullYear() + '-' + z(d.getMonth() + 1) + '-' + z(d.getDate())
+  }
+
   return (
     <div>
       <h1>Hello, Nick!</h1>
       <p>Welcome to your Daily Report Sender.</p>
-      <input type='text' defaultValue={loginCredentials.apiKey} onChange={ChangeAPIKey} />
+      <div>
+        <label>API Key</label>
+        <input type='text' value={loginCredentials.apiKey} onChange={ChangeAPIKey} />
+      </div>
+      <div>
+        <label>API Secret</label>
+        <input type='text' value={loginCredentials.apiSecret} onChange={ChangeAPISecret} />
+      </div>
+      <input type='date' onChange={ChangeChosenDate} />
       <p>{token?.token}</p>
       <button disabled={logInDisabled} onClick={HandleLogin}>Log In</button>
       <button onClick={HandleDailyReport}>Get Daily Report</button>
